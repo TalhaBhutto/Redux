94aaf3035ae42baee648f68aee7708e9f6d5ff58
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-02 00:35:12 +0500

    Update index.js

--- ReduxDemo/index.js
+++ ReduxDemo/index.js
@@ -1,1 +1,27 @@
-console.log("Hello");
+const redux = require('redux');
+const createStore= redux.createStore;
+
+const buy_cake = 'BUY CAKE';
+
+function buyCake(){
+    return{
+    type: buy_cake,
+    info: 'First redux action'}
+}
+
+const initialState={
+    numOfCake=10
+}
+const reducer =(state=initialState,action)=>{
+    switch(action.type){
+        case buy_cake: return{
+            ... state,
+            numOfCake: state.numOfCake-1
+        }
+        default: return state
+    }
+
+}
+
+const store =createStore(reducer);
+
