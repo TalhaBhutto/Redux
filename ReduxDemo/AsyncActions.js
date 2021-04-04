ae366eaa82d618588f3443342c0ee0a1205df273
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-04 23:55:36 +0500

    Update AsyncActions.js

--- ReduxDemo/AsyncActions.js
+++ ReduxDemo/AsyncActions.js
@@ -57,7 +57,7 @@ const reducer = (state=initialState,actions)=>{
 const fetchUsers=()=>{
     return function(dispatch){
         dispatch(fetchUsersRequest())
-        axios.get('https://jsonplaceholder.typicode.com/todos')
+        axios.get('https://jsonplaceholder.typicode.com/users')
         .then(Response=>{
             const users=Response.data.map(user=>user.id)
             dispatch(fetchUsersSuccess(users))
