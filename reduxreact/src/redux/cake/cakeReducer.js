be032f46a745b6a225fd08b78c0155cec3dafa50
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-05 23:40:20 +0500

    Update cakeReducer.js

--- reduxreact/src/redux/cake/cakeReducer.js
+++ reduxreact/src/redux/cake/cakeReducer.js
@@ -1,7 +1,17 @@
+import {BUY_CAKES} from "./CakeTypes"
+
 const initialState={
     numberOfCakes:10,
 }
 
 const cakeReducer=(state=initialState,actions)=>{
-    
+    switch(actions.type){
+        case BUY_CAKES:return{
+            ...state,
+            numberOfCakes:state.numberOfCakes-1
+        }
+        default: return state
+    }
 }
+
+export default cakeReducer
