27e769981d02ec8d373541813eb8db6b51959ded
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-06 23:58:23 +0500

    Update UserReducer.js

--- reduxreact/src/redux/user/UserReducer.js
+++ reduxreact/src/redux/user/UserReducer.js
@@ -1,6 +1,15 @@
+import { FETCH_USER_FAILURE, FETCH_USER_REQUEST, FETCH_USER_SUCCESS } from "./UserTypes"
+
 const initialState={
     loading:false,
     users:[],
     error:''
 }
 
+const Reducer(state=initialState,actions){
+    switch(actions.type){
+        case FETCH_USER_REQUEST:
+        case FETCH_USER_SUCCESS:
+        case FETCH_USER_FAILURE:
+    }
+}
