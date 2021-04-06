72bf5f60ecde4af22384e70d858ddf674690121a
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-07 01:41:38 +0500

    Update UserComponent.js

--- reduxreact/src/components/UserComponent.js
+++ reduxreact/src/components/UserComponent.js
@@ -3,11 +3,23 @@ import {connect} from 'react-redux'
 import {fetchUsers} from '../redux'
 
 function UserComponent({userData,fetchUsers} ) {
-    useEffect(({fetchUsers})=>{
+    useEffect(()=>{
         fetchUsers()
     },[])
-    return userData.loading?(<h2>Loading</h2>):userData.error?(<h2>{userData.error}</h2>):
-    <div><h2>User List</h2><div>{userData && userData.users && userData.users.map(user=><p>{user.name}</p>)}</div></div>
+    return userData.loading?(
+    <h2>Loading</h2>
+    ):userData.error?(
+    <h2>{userData.error}</h2>
+    ):(
+    <div>
+        <h2>User List</h2>
+        <div>
+            {userData && 
+            userData.users && 
+            userData.users.map(user=><p>{user.name}</p>)}
+            </div>
+            </div>
+            )
 }
 
 const mapStateToProps=state=>{
