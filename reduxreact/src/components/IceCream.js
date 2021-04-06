c148bc1ecf20f1b51a53af77db0b9b29dc7fad44
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-06 16:23:25 +0500

    Create IceCream.js

---
+++ reduxreact/src/components/IceCream.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {buyIceCream} from '../redux'
+import {connect} from 'react-redux'
+
+function IceCream(props) {
+    return (
+        <div>
+            <h2>Number of IceCreams - {props.numOfIceCreams}</h2>
+            <button onClick={props.buyIceCream}>Buy IceCream</button>
+        </div>
+    )
+}
+const mapStateToProps=state=>{
+    return{
+        numOfIceCreams:state.numOfIceCreams
+    }
+}
+const mapDispatchToProps=dispatch=>{
+    return{
+        buyIceCream:()=>dispatch(buyIceCream())
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(IceCream)
