2dcfbadb570dc3d13e566518ac44f9e44db85ece
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-06 17:45:55 +0500

    applied middleware

--- reduxreact/src/redux/store.js
+++ reduxreact/src/redux/store.js
@@ -1,6 +1,7 @@
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import RootReducer from "./RootReducer"
+import jogger from 'redux-logger'
 
-const store=createStore(RootReducer)
+const store=createStore(RootReducer,applyMiddleware(jogger))
 
 export default store
