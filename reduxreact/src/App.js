a490223a5ea02d9e182658e877b911b688d36865
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-06 16:04:58 +0500

    Implemented redux via hooks

--- reduxreact/src/App.js
+++ reduxreact/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import store from "./redux/store"
 import Cakes from "./components/cake"
 import {Provider} from 'react-redux'
+import HooksCakeContainer from './HooksCakeContainer';
 
 function App() {
   return (
     <Provider store={store}>
     <div className="App">
+      <HooksCakeContainer></HooksCakeContainer>
       <Cakes></Cakes>
     </div>
     </Provider>
