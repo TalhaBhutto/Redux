890cec6cb72f9f77173256548bbef20e11dcb664
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-08 23:52:28 +0500

    Update UserComponent.js

--- reduxreact/src/components/UserComponent.js
+++ reduxreact/src/components/UserComponent.js
@@ -32,7 +32,7 @@ const mapDispatchToProps = dispatch => {
   return {
     fetchUsers: () => dispatch(fetchUsers())
   }
-}abcdefghijklmno
+}abcdefghijklmnop
 
 export default connect(
   mapStateToProps,
